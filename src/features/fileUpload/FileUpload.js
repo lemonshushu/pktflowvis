8da044ad2a80972b7db856d6aba075db755851fa
Subin Song <subin@subinsong.com>
2024-10-28 16:34:34 +0900

    fix: Fix navigation from FileUpload to GraphView

--- src/features/fileUpload/FileUpload.js
+++ src/features/fileUpload/FileUpload.js
@@ -1,9 +1,15 @@
 import React, { useState } from 'react';
 import { Button, Form } from 'react-bootstrap';
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
+import { Navigate } from 'react-router-dom';
 import { setPackets } from './fileUploadSlice';
 
 
+/**
+ * This function parses a JSON file and returns the parsed object
+ * @param {File} file - The JSON file to be parsed
+ * @returns {Promise<Object>} - The parsed JSON object
+ */
 const parseJson = (file) => {
     return new Promise((resolve, reject) => {
         const reader = new FileReader();
@@ -24,7 +30,13 @@ const parseJson = (file) => {
 export default function FileUpload() {
     const dispatch = useDispatch();
     const [ selectedFile, setSelectedFile ] = useState(null);
+    const packets = useSelector((state) => state.fileUpload.packets);
 
+
+    /**
+     * This function is called when the user clicks the submit button
+     * @param {File} selectedFile - The selected file
+     */
     const onSubmitClick = async (selectedFile) => {
         if (selectedFile != null) {
             try {
@@ -44,12 +56,15 @@ export default function FileUpload() {
 
 
     return (
+        packets ? <Navigate to="/graph" /> :
         <div className="d-flex align-items-center justify-content-center vh-100">
-            <div><Form.Group controlId="formFile" className="mb-3" width="50%">
-                <Form.Label column={true}>Upload your JSON file: </Form.Label>
-                <Form.Control type="file" onChange={(e) => setSelectedFile(e.target.files[ 0 ])} />
-            </Form.Group>
+            <div>
+                <Form.Group controlId="formFile" className="mb-3" width="50%">
+                    <Form.Label column={true}>Upload your JSON file: </Form.Label>
+                    <Form.Control type="file" onChange={(e) => setSelectedFile(e.target.files[ 0 ])} />
+                </Form.Group>
                 <Button variant="primary" type="submit" onClick={() => onSubmitClick(selectedFile)}>Submit</Button>
-            </div></div>
+            </div>
+        </div>
     );
 }
--- src/features/graphView/GraphView.js
+++ src/features/graphView/GraphView.js
@@ -1,7 +1,14 @@
+import React from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { Navigate } from 'react-router-dom';
+
 export default function GraphView() {
+    const packets = useSelector((state) => state.fileUpload.packets);
+    const dispatch = useDispatch();
     return (
-        <div>
-            <h1>Graph View</h1>
-        </div>
+        packets ?
+            <div>
+                <h1>Graph View</h1>
+            </div> : <Navigate to="/" />
     );
 }
