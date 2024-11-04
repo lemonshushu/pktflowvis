02aebe9291fb24b8d69ec6443629637d44586d2d
Subin Song <subin@subinsong.com>
2024-11-04 19:03:35 +0900

    add 틀 for timeline view

--- src/features/timelineView/components/TimelineEntry.js
+++ src/features/timelineView/components/TimelineEntry.js
@@ -1,16 +1,63 @@
-import { CloseButton, Form } from "react-bootstrap";
+import { useState } from "react";
+import { Button, CloseButton, Col, Form, Row } from "react-bootstrap";
 import { useDispatch } from 'react-redux';
 import { removeEntry } from '../timelineViewSlice';
 import "./TimelineEntry.css";
 
 export default function TimelineEntry({ entryIndex }) {
     const dispatch = useDispatch();
+    const [radioASelected, setRadioASelected] = useState(true);
+
+    const onLocalhostChange = (e) => {
+        setRadioASelected(e.target.value === "A");
+    }
+
     return (
         <div className="timeline-entry d-flex align-items-center flex-column mb-3">
             <CloseButton className="align-self-end mt-3 me-3" onClick={() => {
                 dispatch(removeEntry(entryIndex));
             }} />
             <Form.Control placeholder="Entry title" className="entry-title mb-2" />
+            <svg width="80%" height="200px" className="timeline-svg" />
+            <Form className="entry-form p-4">
+                <Row>
+                    <Col xs={5}></Col>
+                    <Col xs={3}></Col>
+                    <Col xs={2}>localhost</Col>
+                    <Col xs={1}></Col>
+                </Row>
+                <Row className="mb-3">
+                    <Col xs={5} className="d-flex align-items-center justify-content-center">
+                        <Form.Label><strong>Host A: </strong></Form.Label>
+                        <Form.Select className="ms-3" style={{ width: 250 }}></Form.Select>
+                    </Col>
+                    <Col xs={3} className="d-flex align-items-center justify-content-center">
+                        <Form.Label>Port: </Form.Label>
+                        <Form.Select className="ms-3" style={{ width: 150 }}></Form.Select>
+                    </Col>
+                    <Col xs={2} className="d-flex align-items-center justify-content-center">
+                        <Form.Check type="radio" name="localhost" value="A" onChange={onLocalhostChange} defaultChecked/>
+                    </Col>
+                    <Col xs={1} className="d-flex align-items-center justify-content-center">
+                        <Button>Save</Button>
+                    </Col>
+                </Row>
+                <Row>
+                    <Col xs={5} className="d-flex align-items-center justify-content-center">
+                        <Form.Label><strong>Host B: </strong></Form.Label>
+                        <Form.Select className="ms-3" style={{ width: 250 }}></Form.Select>
+                    </Col>
+                    <Col xs={3} className="d-flex align-items-center justify-content-center">
+                        <Form.Label>Port: </Form.Label>
+                        <Form.Select className="ms-3" style={{ width: 150 }}></Form.Select>
+                    </Col>
+                    <Col xs={2} className="d-flex align-items-center justify-content-center">
+                        <Form.Check type="radio" name="localhost" value="B" onChange={onLocalhostChange} />
+                    </Col>
+                    <Col xs={1} className="d-flex align-items-center justify-content-center">
+                    </Col>
+                </Row>
+            </Form>
         </div>
     );
 }
--- src/features/timelineView/timelineViewSlice.js
+++ src/features/timelineView/timelineViewSlice.js
@@ -5,12 +5,10 @@ export const timelineViewSlice = createSlice({
     initialState: {
         alignTime: false, // Whether to align the time of multiple timeline entries
         timelineData: [],
-        selections: [], // Selected options for the timeline entries. Keys: host_a, host_b, port_a, port_b, localhost_is_a
     },
     reducers: {
         addEntry: (state, action) => {
             state.timelineData.push([]);
-            state.selections.push({host_a: null, host_b: null, port_a: null, port_b: null, localhost_is_a: true});
         },
         toggleAlignTime: (state) => {
             state.alignTime = !state.alignTime;
@@ -18,7 +16,6 @@ export const timelineViewSlice = createSlice({
         removeEntry: (state, action) => {
             // action.payload is entry index
             state.timelineData.splice(action.payload, 1);
-            state.selections.splice(action.payload, 1);
         },
     },
 });
