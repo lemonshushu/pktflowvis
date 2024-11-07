97dbb88e563e7d08f64633cb72a2119f9d310892
Subin Song <subin@subinsong.com>
2024-11-07 12:01:18 +0900

    fix: Refactor dropdown selections to global state

--- src/features/timelineView/components/TimelineEntry.js
+++ src/features/timelineView/components/TimelineEntry.js
@@ -1,43 +1,60 @@
-import { useState } from "react";
 import { Button, CloseButton, Col, Form, Row } from "react-bootstrap";
 import { useDispatch, useSelector } from 'react-redux';
 import { removeEntry } from '../timelineViewSlice';
 import "./TimelineEntry.css";
+import { setFormSelections } from '../timelineViewSlice';
 
 export default function TimelineEntry({ entryIndex }) {
     const dispatch = useDispatch();
     const timelineViewOpts = useSelector((state) => state.timelineView.timelineViewOpts);
+    const formSelections = useSelector((state) => state.timelineView.formSelections);
+    const formSelection = formSelections[entryIndex];
 
-    const [ hostA, setHostA ] = useState("");
-    const [ hostAIndex, setHostAIndex ] = useState(null);
-    const [ portA, setPortA ] = useState("");
-    const [ hostB, setHostB ] = useState("");
-    const [ hostBIndex, setHostBIndex ] = useState(null);
-    const [ portB, setPortB ] = useState("");
-    const [ radioASelected, setRadioASelected ] = useState(true);
+    // const [ hostA, setHostA ] = useState("");
+    // const [ hostAIndex, setHostAIndex ] = useState(null);
+    // const [ portA, setPortA ] = useState("");
+    // const [ hostB, setHostB ] = useState("");
+    // const [ hostBIndex, setHostBIndex ] = useState(null);
+    // const [ portB, setPortB ] = useState("");
+    // const [ radioASelected, setRadioASelected ] = useState(true);
 
 
     const onHostAChange = (e) => {
-        setHostA(e.target.value);
-        setHostAIndex(timelineViewOpts.findIndex((opt) => opt.ip_addr === e.target.value));
+        // setHostA(e.target.value);
+        // setHostAIndex(timelineViewOpts.findIndex((opt) => opt.ip_addr === e.target.value));
+        // dispatch(setFormSelections({...formSelections, hostA: e.target.value, hostAIndex: timelineViewOpts.findIndex((opt) => opt.ip_addr === e.target.value)}));
+        dispatch(setFormSelections({index: entryIndex, selection: {...formSelection, hostA: e.target.value, hostAIndex: timelineViewOpts.findIndex((opt) => opt.ip_addr === e.target.value)} }));
     };
 
     const onPortAChange = (e) => {
-        setPortA(e.target.value);
+        // setPortA(e.target.value);
+        // dispatch(setFormSelections({...formSelections, portA: e.target.value}));
+        dispatch(setFormSelections({index: entryIndex, selection: {...formSelection, portA: e.target.value}}));
     };
 
     const onHostBChange = (e) => {
-        setHostB(e.target.value);
-        setHostBIndex(timelineViewOpts.findIndex((opt) => opt.ip_addr === e.target.value));
+        // setHostB(e.target.value);
+        // setHostBIndex(timelineViewOpts.findIndex((opt) => opt.ip_addr === e.target.value));
+        // dispatch(setFormSelections( {...formSelections,hostB: e.target.value, hostBIndex: timelineViewOpts.findIndex((opt) => opt.ip_addr === e.target.value)}));
+        dispatch(setFormSelections({index: entryIndex, selection:  {...formSelection,hostB: e.target.value, hostBIndex: timelineViewOpts.findIndex((opt) => opt.ip_addr === e.target.value)} }));
     };
 
     const onPortBChange = (e) => {
-        setPortB(e.target.value);
+        // setPortB(e.target.value);
+        // dispatch(setFormSelections( {...formSelections,portB: e.target.value}));
+        dispatch(setFormSelections({index: entryIndex, selection: {...formSelection,portB: e.target.value}}));
     };
 
     const onRadioChange = (e) => {
-        setRadioASelected(e.target.value === "A");
+        // setRadioASelected(e.target.value === "A");
+        // dispatch(setFormSelections( {...formSelections,radioASelected: e.target.value === "A"}));
+        dispatch(setFormSelections({index: entryIndex, selection: {...formSelection,radioASelected: e.target.value === "A"} }));
     };
+
+    const onResetClick = () => {
+        // dispatch(setFormSelections( {hostA: "", hostAIndex: null, portA: "", hostB: "", hostBIndex: null, portB: "", radioASelected: true}));
+        dispatch(setFormSelections({index: entryIndex, selection: {hostA: "", hostAIndex: null, portA: "", hostB: "", hostBIndex: null, portB: "", radioASelected: true}}));
+    }
 
     return (
         <div className="timeline-entry d-flex align-items-center flex-column mb-3">
@@ -56,7 +73,7 @@ export default function TimelineEntry({ entryIndex }) {
                 <Row className="mb-3">
                     <Col xs={5} className="d-flex align-items-center justify-content-center">
                         <Form.Label><strong>Host A: </strong></Form.Label>
-                        <Form.Select className="ms-3" style={{ width: 250 }} onChange={onHostAChange}>
+                        <Form.Select className="ms-3" style={{ width: 250 }} onChange={onHostAChange} value={formSelection.hostA}>
                             <option></option>
                             {timelineViewOpts.map((opt, index) => {
                                 return (<option key={index}>{opt.ip_addr}</option>);
@@ -65,24 +82,24 @@ export default function TimelineEntry({ entryIndex }) {
                     </Col>
                     <Col xs={3} className="d-flex align-items-center justify-content-center">
                         <Form.Label>Port: </Form.Label>
-                        <Form.Select className="ms-3" style={{ width: 150 }} onChange={onPortAChange}>
-                            {timelineViewOpts[ hostAIndex ] ? timelineViewOpts[ hostAIndex ].ports.map((port, index) => {
+                        <Form.Select className="ms-3" style={{ width: 150 }} onChange={onPortAChange} value={formSelection.portA}>
+                            {timelineViewOpts[ formSelection.hostAIndex ] ? timelineViewOpts[ formSelection.hostAIndex ].ports.map((port, index) => {
                                 return (<option key={index}>{port}</option>);
                             }) : null
                             }
                         </Form.Select>
                     </Col>
                     <Col xs={2} className="d-flex align-items-center justify-content-center">
-                        <Form.Check type="radio" name="localhost" value="A" onChange={onRadioChange} defaultChecked />
+                        <Form.Check type="radio" name="localhost" value="A" onChange={onRadioChange} checked={formSelection.radioASelected} />
                     </Col>
                     <Col xs={1} className="d-flex align-items-center justify-content-center">
-                        <Button>Save</Button>
+                        <Button variant="light" onClick={onResetClick}>Reset</Button>
                     </Col>
                 </Row>
                 <Row>
                     <Col xs={5} className="d-flex align-items-center justify-content-center">
                         <Form.Label><strong>Host B: </strong></Form.Label>
-                        <Form.Select className="ms-3" style={{ width: 250 }} onChange={onHostBChange}>
+                        <Form.Select className="ms-3" style={{ width: 250 }} onChange={onHostBChange} value={formSelection.hostB}>
                             <option></option>
                             {timelineViewOpts.map((opt, index) => {
                                 return (<option key={index}>{opt.ip_addr}</option>);
@@ -90,17 +107,18 @@ export default function TimelineEntry({ entryIndex }) {
                     </Col>
                     <Col xs={3} className="d-flex align-items-center justify-content-center">
                         <Form.Label>Port: </Form.Label>
-                        <Form.Select className="ms-3" style={{ width: 150 }} onChange={onPortBChange}>
-                            {timelineViewOpts[ hostBIndex ] ? timelineViewOpts[ hostBIndex ].ports.map((port, index) => {
+                        <Form.Select className="ms-3" style={{ width: 150 }} onChange={onPortBChange} value={formSelection.portB}>
+                            {timelineViewOpts[ formSelection.hostBIndex ] ? timelineViewOpts[ formSelection.hostBIndex ].ports.map((port, index) => {
                                 return (<option key={index}>{port}</option>);
                             }) : null
                             }
                         </Form.Select>
                     </Col>
                     <Col xs={2} className="d-flex align-items-center justify-content-center">
-                        <Form.Check type="radio" name="localhost" value="B" onChange={onRadioChange} />
+                        <Form.Check type="radio" name="localhost" value="B" onChange={onRadioChange} checked={!formSelection.radioASelected} />
                     </Col>
                     <Col xs={1} className="d-flex align-items-center justify-content-center">
+                    <Button>Save</Button>
                     </Col>
                 </Row>
             </Form>
--- src/features/timelineView/timelineViewSlice.js
+++ src/features/timelineView/timelineViewSlice.js
@@ -6,10 +6,12 @@ export const timelineViewSlice = createSlice({
         alignTime: false, // Whether to align the time of multiple timeline entries
         timelineData: [],
         timelineViewOpts: [],
+        formSelections: [],
     },
     reducers: {
         addEntry: (state, action) => {
             state.timelineData.push([]);
+            state.formSelections.push({hostA: "", hostAIndex: null, portA: "", hostB: "", hostBIndex: null, portB: "", radioASelected: true});
         },
         toggleAlignTime: (state) => {
             state.alignTime = !state.alignTime;
@@ -21,9 +23,13 @@ export const timelineViewSlice = createSlice({
         setTimelineViewOpts: (state, action) => {
             state.timelineViewOpts = action.payload;
         },
+        setFormSelections: (state, action) => {
+            const {index, selection} = action.payload;
+            state.formSelections[index] = selection;
+        },
     },
 });
 
-export const { addEntry, toggleAlignTime, removeEntry, setTimelineViewOpts } = timelineViewSlice.actions;
+export const { addEntry, toggleAlignTime, removeEntry, setTimelineViewOpts, setFormSelections } = timelineViewSlice.actions;
 
 export default timelineViewSlice.reducer;
